5317206ee6da2596b083d901fa29459b17139f2d
Stefanos <stefveisak@gmail.com>
2022-05-16 11:09:53 +0300

    parse .txt into array

--- ids.c
+++ ids.c
@@ -36,14 +36,50 @@ int main(int argc, char **argv) {
     char errbuf[PCAP_ERRBUF_SIZE];
     char source[PCAP_BUF_SIZE];
     int i, maxCountSyn = 0, maxCountHttp = 0, maxIdxSyn = 0, maxIdxHttp = 0;
+    char lines[100][5][2048];
+    int tot = 0;
+    char line[1024];
 
-    if(argc != 2) {
-        printf("usage: %s filename\n", argv[0]);
+    if(argc != 3) {
+        printf("usage: %s <.pcap> <.txt> \n", argv[0]);
         return -1;
     }
 
+    FILE *f = fopen(argv[2], "r");
 
-    /*fp = pcap_open_offline_with_tstamp_precision(argv[0], PCAP_TSTAMP_PRECISION_NANO, errbuf);*/
+    while(fgets(line, sizeof(line) / sizeof(line[0]), f) != NULL)
+    {
+
+        if (strchr(line, '\n') == NULL) {
+            printf("Line too long...");
+            return EXIT_FAILURE;
+        }
+
+        char *ptr1 = strtok(line, " ");
+        strcpy(lines[i][0], ptr1);
+        char *ptr2 = strtok(NULL, " ");
+        strcpy(lines[i][1], ptr2);
+        char *ptr3 = strtok(NULL, " ");
+        strcpy(lines[i][2], ptr3);
+        char *ptr4 = strtok(NULL, " ");
+        strcpy(lines[i][3], ptr4);
+        char *ptr5 = strtok(NULL, "\"");
+        strcpy(lines[i][4], ptr5);
+        i++;
+    }
+
+    tot=i;
+
+    for (int i=0;i<tot;i++)
+    {
+        printf("first value %s\n", lines[i][0]);
+        printf("second value is %s\n", lines[i][1]);
+        printf("third value %s\n", lines[i][2]);
+        printf("fourth value is %s\n", lines[i][3]);
+        printf("fiveth value is %s\n", lines[i][4]);
+        printf("\n");
+    }
+    
     fp = pcap_open_offline(argv[1], errbuf);
     if (fp == NULL) {
 	    fprintf(stderr, "\npcap_open_offline() failed: %s\n", errbuf);
@@ -51,7 +87,7 @@ int main(int argc, char **argv) {
     }
 
 
-    if (pcap_loop(fp, 0, packetHandler, NULL) < 0) {
+    if (pcap_loop(fp, 100, packetHandler, NULL) < 0) {
         fprintf(stderr, "\npcap_loop() failed: %s\n", pcap_geterr(fp));
         return 0;
     }
